7f575539e7f0dce847277299512b23c5fec6b62e
hiro <hironori.matsuse@gmail.com>
2019-10-14 11:13:14 +0900

    Add:test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_023058) do
+ActiveRecord::Schema.define(version: 2019_09_19_030841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "matsutters", force: :cascade do |t|
     t.text "content"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
